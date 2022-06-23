7e2e0c924b80d01f92b0968af02c3258eb03426c
shpilb <shpilb@Dell.com>
2022-06-23 17:45:38 +0300

    User model refactor + new version

--- test/testuser.go
+++
@@ -1,9 +0,0 @@
-package mytest
-
-type TestUser struct {
-	Id             string `json:"userid"`
-	Name           string `json:"username"`
-	Department     string `json:"department"`
-	Salary         int    `json:"salary"`
-	DepartmentCode string `json:"departmentcode"`
-}
--- usersdata.go
+++ usersdata.go
@@ -1,8 +1,8 @@
-package usrdata
+package usersrdata
 
 type User struct {
-	Id         string `json:"userid"`
-	Name       string `json:"username"`
-	Department string `json:"department"`
-	Salary     int    `json:"salary"`
+	Id     string `json:"userid"`
+	Name   string `json:"username"`
+	Unit   string `json:"unit"`
+	Salary int    `json:"salary"`
 }
